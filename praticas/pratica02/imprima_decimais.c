981cd321156637334508e78a25b4a5334b3dcb4d
Matheus <matheusiillva39@gmail.com>
2026-04-01 21:51:01 -0300

    feat: conclui pratica02. Fecha #4

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){
+printf("%.7f\n %.2f\n ", 3.1415926, 3.1415926);
+
+return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){
+printf("%u\n %d\n %li\n", 10, -5, 0004 );
+
+return 0;
+}
